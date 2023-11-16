c4e2dab35d6cffdd4f731beadf003710ca69b0bb
Dharmesh Basapati Bacancy <dharmesh.basapati@bacancy.com>
2023-11-16 16:17:02 +0530

    Added logger related extension functions.

--- OurBaseUtils/src/main/java/com/example/ourbaseutils/logging/Logger.kt
+++ OurBaseUtils/src/main/java/com/example/ourbaseutils/logging/Logger.kt
@@ -1,6 +1,7 @@
 package com.example.ourbaseutils.logging
 
 import android.content.Context
+import android.util.Log
 import android.widget.Toast
 
 fun Context.showShortToast(message: String): Toast {
@@ -10,3 +11,13 @@ fun Context.showShortToast(message: String): Toast {
 fun Context.showLongToast(message: String): Toast {
     return Toast.makeText(this, message, Toast.LENGTH_LONG).apply { show() }
 }
+
+fun Any.showVLog(log: String) = Log.v(this::class.java.simpleName, log)
+
+fun Any.showELog(log: String) = Log.e(this::class.java.simpleName, log)
+
+fun Any.showDLog(log: String) = Log.d(this::class.java.simpleName, log)
+
+fun Any.showILog(log: String) = Log.i(this::class.java.simpleName, log)
+
+fun Any.showWLog(log: String) = Log.w(this::class.java.simpleName, log)
